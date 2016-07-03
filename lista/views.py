4b0c474e622274c4e893609e6ae4588df4e656cd
Adam <adam.golubowski@gmail.com>
2016-07-03 13:11:54 +0000

    first view added

--- lista/views.py
+++ lista/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def items_list(request):
+    items = Item.objects.all()
+    return render(request,'items_list.html',{'items':items})
