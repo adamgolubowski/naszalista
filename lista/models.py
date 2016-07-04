1579ccfb0934c63cf672129ac1bcdd811caab24e
Adam <adam.golubowski@gmail.com>
2016-07-04 16:43:08 +0000

    updated media folder and urls settings

--- lista/models.py
+++ lista/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField(blank=True)
     donor=models.CharField(max_length=200, blank=True)
-    photo=models.ImageField(upload_to='static/items', default='static/items/book_generic.png')
+    photo=models.ImageField(default='book_generic.png')
     
     def take(self,newDonor):
         self.donor=newDonor
--- naszalista/.~c9_invoke_Kb1NxC.py
+++ naszalista/.~c9_invoke_Kb1NxC.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open(os.path.expanduser('./naszalista/secretkey')).read().strip()
+from settings_secret import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Warsaw'
 
@@ -122,9 +122,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
