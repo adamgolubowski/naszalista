513c882504c8166d4190453265ee29a832bc8998
Adam <adam.golubowski@gmail.com>
2016-08-16 17:49:52 +0000

    added upload_to paramter to the imageField

--- lista/models.py
+++ lista/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField(blank=True)
     donor=models.CharField(max_length=200, blank=True)
-    photo=models.ImageField(default='book_generic.png')
+    photo=models.ImageField(default='book_generic.png', upload_to='.')
     
     def take(self,newDonor):
         self.donor=newDonor
